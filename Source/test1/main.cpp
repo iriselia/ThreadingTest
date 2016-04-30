db169833b2b16817a35689128a5d075640ee059e
Frank Park <y.piao.us@ieee.org>
2016-04-30 18:19:46 -0400

    ....

--- Source/test1/main.cpp
+++ Source/test1/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <mutex>
 #include <fstream>
+#include <future>
 #include "class.h"
 
 using namespace std;
@@ -27,7 +28,10 @@ public:
 		_f.open("log.txt");
 	}
 
-	~LogFile();
+	~LogFile()
+	{
+
+	}
 	void shared_print(std::string msg, int id)
 	{
 		std::lock_guard<mutex> locker(_mu);
@@ -38,21 +42,78 @@ private:
 
 };
 
-LogFile::LogFile()
+class Factorial
 {
+public:
 
+	int operator()(int num)
+	{
+		for (int i = 1; i <= num; i++)
+		{
+			num = num * i;
+		}
+		return num;
+	}
+
+	int memberfunc(int num)
+	{
+		return this->operator()(num);
+	}
+private:
+
+};
+
+int factorial(int num)
+{
+	for (int i = 1; i <= num; i++)
+	{
+		num = num * i;
+	}
+	return num;
 }
-
-LogFile::~LogFile()
-{}
 
 int main()
 {
+	Factorial f;
+	/* thread invocation template */
+	std::thread t1(f, 6); //copy of f invoked in new thread
+	std::thread t2(std::ref(f), 6); // ref of f in main thread
+	std::thread t3(std::move(f), 6); // turn f into a rvalue ref and move construct it in new thread
+	std::thread t4(Factorial(), 6); // temp factorial in both main and new thread (? unsure)
+	std::thread t5([](int x) { return x*x; }, 6); // call lambda
+	std::thread t6(factorial, 6); // lower case factorial, global function
+	std::thread t7(&Factorial::memberfunc, f, 6); // memberfunc invoked on a copy of f
+	std::thread t8(&Factorial::memberfunc, &f, 6); // memberfunc invoked on ref of f
+
+
+	/* mutex */
+	std::mutex mu;
+	{
+		std::lock_guard<mutex> locker(mu);
+	}
+	std::unique_lock<mutex> ulocker(mu);
+
+	/* condition variable */
+	std::condition_variable cond;
+
+	/* future and promise */
+	std::promise<int> p;
+	std::future<int> fu = p.get_future();
+
+	/* async() */
+	std::future<int> fu2 = async(factorial, 6);
+
+	/* packaged task */
+	std::packaged_task<int(int)> t(factorial);
+	std::future<int> fu3 = t.get_future();
+	t(6);
+
+
 	cout << std::this_thread::get_id() << endl;
 
 	string str = "idk what to do";
-	std::thread t1((Functor()), std::move(str));
-	cout << t1.get_id() << endl;
+	//std::thread t1((Functor()), std::move(str));
+	//cout << t1.get_id() << endl;
 
 	cout << std::thread::hardware_concurrency() << endl; // max threads on current system
 
@@ -78,5 +139,12 @@ int main()
 	{
 		t1.join(); // will crash without test
 	}
+	t2.join();
+	t3.join();
+	t4.join();
+	t5.join();
+	t6.join();
+	t7.join();
+	t8.join();
 	return 0;
 }
